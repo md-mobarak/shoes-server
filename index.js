1066af577899b4f02e3ca660bfcdd8c85f985337
mohammad-mobarak-hossen <mdmobarakhossen112@gmail.com>
2022-09-14 12:35:55 +0600

    post api is ready

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ require('dotenv').config()
 app.use(express.json())
 app.use(cors())
 
+// DB_USER=mobarakDb
+// DB_PASSWORD=9iSPNWnnXkFvqOvG
 
-
-const uri = "mongodb+srv://mobarakDb:9iSPNWnnXkFvqOvG@cluster0.itqkdxk.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.itqkdxk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
@@ -25,6 +26,29 @@ async function run(params) {
         app.get('/product', async (req, res) => {
             const product = await productCollection.find().toArray()
             res.send(product)
+        })
+
+
+        app.delete('/product/:id', async (req, res) => {
+            const { id } = req.params;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query)
+            res.send(result)
+        })
+
+
+        app.get('/product/:id', async (req, res) => {
+            const { id } = req.params;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.findOne(query)
+            res.send(result)
+        })
+
+
+        app.post('/addProduct', async (req, res) => {
+            const data = req.body;
+            const result = await productCollection.insertOne(data)
+            res.send(result)
         })
 
     }
